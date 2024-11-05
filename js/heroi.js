20cd342ed4001a8c7b50a4d2534ba381ce44a67e
MarceloKobayashi <marcelohk2004@gmail.com>
2024-11-05 15:20:55 -0300

    1105 - não fui cortar cabelo.

--- js/heroi.js
+++ js/heroi.js
@@ -1,23 +1,29 @@
 const dialogCadastrar = document.getElementById("dialog-cadastrar");
-
 const btnCadastrar = document.getElementById("btn-cadastrar");
 btnCadastrar.addEventListener("click", () => {
     dialogCadastrar.showModal();
 });
-
 const btnCadastroFechar = document.getElementById("btn-dialog-cadastrar-fechar");
 btnCadastroFechar.addEventListener("click", () => {
     dialogCadastrar.close();
 });
 
 const dialogExcluir = document.getElementById("dialog-excluir");
-
 const btnExcluir = document.getElementById("btn-excluir");
 btnExcluir.addEventListener("click", () => {
     dialogExcluir.showModal();
 });
-
 const btnExclusaoFechar = document.getElementById("btn-dialog-excluir-fechar");
 btnExclusaoFechar.addEventListener("click", () => {
     dialogExcluir.close();
+});
+
+const dialogAlterar = document.getElementById("dialog-alterar");
+const btnAlterar = document.getElementById("btn-alterar");
+btnAlterar.addEventListener("click", () => {
+    dialogAlterar.showModal();
+});
+const btnAlteracaoFechar = document.getElementById("btn-dialog-alterar-fechar");
+btnAlteracaoFechar.addEventListener("click", () => {
+    dialogAlterar.close();
 });
