9221ad2ee18ccb29ce7a29fea0dfbdddd5519e48
MarceloKobayashi <marcelohk2004@gmail.com>
2024-11-05 15:03:58 -0300

    1105 - vou cortar o cabelo

--- js/heroi.js
+++ js/heroi.js
@@ -5,7 +5,19 @@ btnCadastrar.addEventListener("click", () => {
     dialogCadastrar.showModal();
 });
 
-const btnFechar = document.getElementById("btn-dialog-cancelar");
-btnFechar.addEventListener("click", () => {
+const btnCadastroFechar = document.getElementById("btn-dialog-cadastrar-fechar");
+btnCadastroFechar.addEventListener("click", () => {
     dialogCadastrar.close();
+});
+
+const dialogExcluir = document.getElementById("dialog-excluir");
+
+const btnExcluir = document.getElementById("btn-excluir");
+btnExcluir.addEventListener("click", () => {
+    dialogExcluir.showModal();
+});
+
+const btnExclusaoFechar = document.getElementById("btn-dialog-excluir-fechar");
+btnExclusaoFechar.addEventListener("click", () => {
+    dialogExcluir.close();
 });
